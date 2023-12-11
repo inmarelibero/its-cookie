0fb4289d748eb948a8d53c05acdfce497021c2f6
Emanuele Gaspari <inmarelibero@gmail.com>
2023-12-11 10:12:46 +0100

    added solution

--- ex1.php
+++ ex1.php
@@ -7,11 +7,44 @@
  * 3) Stampare la stringa "yellow, red, green, white"
  */
 
-$colors = ['white', 'Green', 'red', 'yellow', 'orange'];
-$separator = ', ';
-echo printColors($colors, $separator);
 
-function printColors()
+function printColors(array $input): string
 {
-//    ...
+    $output = '';
+
+    foreach ($input as $index => $color) {
+        if (!in_array($color, ['white', 'red', 'yellow'])) {
+            continue;
+        }
+
+        $output .= $color;
+
+        if ($index >= count($input) - 1) {
+            break;
+        }
+
+        $output = $output . ', ';
+    }
+
+    return $output;
 }
+
+$colors = ['white', 'Green', 'red', 'yellow', 'orange', 'red'];
+$separator = ', ';
+$colorsString = printColors($colors);
+?>
+
+<html>
+    <body>
+        <p>
+            <b>
+                <?php echo $colorsString; ?>
+            </b>
+        </p>
+        <p>
+            <b>
+                <?php echo $colorsString; ?>
+            </b>
+        </p>
+    </body>
+</html>
