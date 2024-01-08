3692879353caab6094b7f866389f6fd33866716b
Emanuele Gaspari <inmarelibero@gmail.com>
2024-01-08 12:30:47 +0100

    added pages

--- config.test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const APP_DATABASE_HOST = '127.0.0.1';
-const APP_DATABASE_PORT = '3306';
-const APP_DATABASE_USER = 'root';
-const APP_DATABASE_PWD = 'root';
-const APP_DATABASE_NAME = 'its_logistica_test';
