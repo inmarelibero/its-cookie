c479b3b418be08292582d6aeee7866aa476de98f
Emanuele Gaspari <inmarelibero@gmail.com>
2024-02-09 12:27:09 +0100

    fixed password

--- demo-platform/homepage.php
+++ demo-platform/homepage.php
@@ -1,4 +1,17 @@
-<?php require_once('init.php'); ?>
+<?php
+require_once('init.php');
+
+
+// sapere data/ora
+// sapere path della pagina
+
+// costruisco la stringa log da scrivere nel file logs.txt
+
+// creare il file logs.txt se non esiste
+
+// aggiungere il log al file logs.txt
+
+?>
 
 <html>
     <head>
